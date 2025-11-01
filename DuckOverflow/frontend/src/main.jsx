bbd05a03897c4228b093934420e1c01887c77139
TheExtraordinaire07 <nsc7606@rit.edu>
2025-11-01 13:25:17 -0400

    stash change

--- DuckOverflow/frontend/src/main.jsx
+++ DuckOverflow/frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function HelloWorld()
+{
+  return <h1 className ="greeting">Hello, world!</h1>;
+}
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>,
+);
