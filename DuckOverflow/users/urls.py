9ceef4aa1525d4b14f2f2160045b1c25c399b616
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2025-11-01 13:13:59 -0400

    added user model and serializer

--- DuckOverflow/users/urls.py
+++ DuckOverflow/users/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path, include
+from .views import get_user_data
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 url_patterns = [
-]
+    path('user_data/<str:pk>/', get_user_data),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
