09696f45bb559cc66227a2f299984e3ac1a5f5d7
TheExtraordinaire07 <nsc7606@rit.edu>
2025-11-01 15:56:45 -0400

    test output (helloworld)

--- DuckOverflow/frontend/src/main.jsx
+++ DuckOverflow/frontend/src/main.jsx
@@ -1,17 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-function HelloWorld()
-{
-  return <h1 className ="greeting">Hello, world!</h1>;
-}
-
-
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HelloWorld />
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
