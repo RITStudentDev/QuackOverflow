aad6b7f2cf1ba539b5ab8b92da397a566500892e
Vincent Vue <vuevincent2025@gmail.com>
2025-11-01 15:12:32 -0400

    added temporary landing page for django

--- DuckOverflow/DuckOverflow/settings.py
+++ DuckOverflow/DuckOverflow/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'users',
     'posts',
+    '_temporaryhome',
 ]
 
 MIDDLEWARE = [
--- DuckOverflow/DuckOverflow/urls.py
+++ DuckOverflow/DuckOverflow/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from _temporaryhome import views as temp_home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('', temp_home_views.render_home),
+    #path('api/', include('users.urls')),
 ]
