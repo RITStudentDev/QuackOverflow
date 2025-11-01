9f139173ebd6306d220357e770335eec3f324ce1
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2025-11-01 16:43:01 -0400

    added endpoints for rest api converted mainapi to default function

--- DuckOverflow/frontend/src/main.jsx
+++ DuckOverflow/frontend/src/main.jsx
@@ -3,15 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-function HelloWorld()
-{
-  return <h1 className ="greeting">Hello, world!</h1>;
-}
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    <App/>
   </React.StrictMode>,
 );
