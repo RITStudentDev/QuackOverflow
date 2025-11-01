a59bea0ee780d5fd4cf16bbd68ca06c22c9028ed
jcb8333-glitch <jcb8333@g.rit.edu>
2025-11-01 13:16:47 -0400

    added user model and serializer (#2)
    Co-authored-by: Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>

--- DuckOverflow/users/views.py
+++ DuckOverflow/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def get_user_data(request, pk):
+    try:
+        try:
+            user = CustomUser.objects.get(username=pk)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=404)
+
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
+    except:
+        return Response({'error':'error retrieving data'}, status=500)
+
