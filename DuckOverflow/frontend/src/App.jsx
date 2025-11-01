7e4847a32d4c85d4f08edc194204657821f14cb7
Vincent <144172972+user01377@users.noreply.github.com>
2025-11-01 17:47:01 -0400

    merge ai-fix branch (#10)
    fixed bugs with front end not appearing, api still needs fixed
    
    Co-authored-by: Clouu0 <clouvou0@gmail.com>

--- DuckOverflow/frontend/src/App.jsx
+++ DuckOverflow/frontend/src/App.jsx
@@ -7,9 +7,10 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
+    
     <Router>
       <Routes>
-        <Route path="/" element={<HelloWorld/>}/>
+        <Route path="/" element={<Gemini/>}/>
       </Routes>
     </Router>
 
