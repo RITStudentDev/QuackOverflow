2e3a93abeaf0875febdb51f3e12623a0dd4b6451
TheExtraordinaire07 <nsc7606@rit.edu>
2025-11-01 20:42:55 -0400

    implemented duck mascot and main text

--- DuckOverflow/frontend/src/components/QuackOverflow.jsx
+++ DuckOverflow/frontend/src/components/QuackOverflow.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../styles/QuackOverflow.css';
-import duckImage from '../assets/duck.png';
+import duckImage from '../assets/duckHero.png';
 // import App from './App.jsx';
 
 export default function QuackOverflow()
