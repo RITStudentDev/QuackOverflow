27f752845b2191b282fa76b156a7ec205babfcc3
Vincent Vue <vuevincent2025@gmail.com>
2025-11-02 02:04:23 -0500

    temporary change to postconsumer class for merge

--- DuckOverflow/posts/consumers.py
+++ DuckOverflow/posts/consumers.py
@@ -2,41 +2,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 class PostConsumer(AsyncWebsocketConsumer):
-    group_name = "posts"
-
-    async def connect(self):
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.accept()
-        await self.send(text_data=json.dumps({"message": "Connected to posts group!"}))
-        print("WebSocket connected")
-
-    async def disconnect(self, close_code):
-        # Remove from the group on disconnect
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
-        print("WebSocket disconnected")
-
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        message = data.get("message", "")
-
-        print("Received from client:", message)
-
-        # Broadcast message to all members of the group
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                "type": "chat.message",  # type is the name of the handler
-                "message": message,
-            }
-        )
-
-    # This handles messages sent to the group
-    async def chat_message(self, event):
-        message = event["message"]
-        await self.send(text_data=json.dumps({"message": message}))
-
-
-"""class PostConsumer(AsyncWebsocketConsumer):
     
     async def connect(self):
         self.group_name = "posts"
@@ -64,4 +29,3 @@ class PostConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             "message": message,
         }))
-"""
