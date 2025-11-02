ef0c6820f2c830d0ecaab7ee637e8d76ac0be2a9
jcb8333-glitch <jcb8333@g.rit.edu>
2025-11-01 22:36:16 -0400

    Dev/webpage front/naashon (#19)
    * init commit for my branch
    
    * stash change
    
    * test output (helloworld)
    
    * got a working frontend
    
    * implemented duck mascot and main text
    
    * most updated version of front page
    
    ---------
    
    Co-authored-by: TheExtraordinaire07 <nsc7606@rit.edu>

--- DuckOverflow/frontend/src/App.jsx
+++ DuckOverflow/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import Gemini from './mod/mainapi.jsx'
 import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx';
+import QuackOverflow from './components/QuackOverflow.jsx';
 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 //<Route path="/" element={<HelloWorld/>}/>
@@ -13,8 +14,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup/>}/>
-        <Route path="/" element={<Gemini/>}/>
+        <Route path="/mainapi" element={<Gemini/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<QuackOverflow/>}/>
       </Routes>
     </Router>
 
