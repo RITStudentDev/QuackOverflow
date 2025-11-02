20a62d6f65d810d2dd8c09ce4e04cb0f503a8524
jcb8333-glitch <jcb8333@g.rit.edu>
2025-11-01 22:34:34 -0400

    Dev/login jake (#18)
    * added login page
    
    * login finsihed
    
    * set up serializer
    
    ---------
    
    Co-authored-by: Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>

--- DuckOverflow/posts/views.py
+++ DuckOverflow/posts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_post(request):
+    custom_user = get_user_model()
+    data = request.data
+
+    try:
+        user = custom_user.objects.get(username=request.user)
+    except custom_user.DoesNotExist:
+        return Response({'error':'user does not exist'})
+    
+    post = Post.objects.create(
+        user=user,
+        title=request.data  
+    )
+
