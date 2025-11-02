b0d0473387af7bc21ebd05ea493145746c8f4cc6
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2025-11-01 23:19:40 -0400

    added model and serializer to create posts

--- DuckOverflow/DuckOverflow/urls.py
+++ DuckOverflow/DuckOverflow/urls.py
@@ -7,6 +7,7 @@ from users.views import CustomTokenObtainPairView, CustomTokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', temp_home_views.render_home),
+    path('api/', include('posts.urls')),
     path('api/', include('users.urls')),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
---
+++ DuckOverflow/posts/urls.py
@@ -0,0 +1,6 @@
+from .views import create_post
+from django.urls import path
+
+urlpatterns = [
+    path('create_post/', create_post, name='create_post'),
+]
