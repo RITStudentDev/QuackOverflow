dcd86f9c3883d4d3ec8ea4a61afe36e20123e636
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2025-11-02 00:08:44 -0400

    added paginator for posts

--- DuckOverflow/posts/urls.py
+++ DuckOverflow/posts/urls.py
@@ -1,6 +1,7 @@
-from .views import create_post
+from .views import create_post, get_posts
 from django.urls import path
 
 urlpatterns = [
     path('create_post/', create_post, name='create_post'),
+    path('posts/', get_posts, name='get_posts'),
 ]
